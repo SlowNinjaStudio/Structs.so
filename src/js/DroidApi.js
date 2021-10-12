02c26d7a9fc21a66af5de24584f412b98d2dfefb
netlag <n3tlag@outlook.com>
2021-10-12 11:57:42 -0400

    Added dev mode and load testing

--- src/js/DroidApi.js
+++ src/js/DroidApi.js
@@ -5,19 +5,35 @@ import {StructureFactory} from "./StructureFactory";
  * API Gateway for the Droid API
  */
 export class DroidApi {
-  constructor(scheme, domain, ajax) {
+  constructor(scheme, domain, ajax, loadTestMultiply = 1) {
     this.scheme = scheme;
     this.domain = domain;
     this.ajax = (ajax !== undefined) ? ajax: new JsonAjaxer();
+    this.loadTestMultiply = loadTestMultiply;
+  }
+  multiplyDataLoad(dataArray, multiplier) {
+    let product = [];
+    for (let j = 0; j < multiplier; j++) {
+      product = product.concat(dataArray);
+    }
+    return product;
   }
   getStructures() {
     return this.ajax.get(`${this.scheme}${this.domain}/api/di/Structure`)
       .then(data => {
         const structureFactory = new StructureFactory();
         const structures = [];
-        for (let i = 0; i < data.Structure.length; i ++) {
-          structures[i] = structureFactory.make(data.Structure[i]);
+        let rawStructures = data.Structure;
+
+        if (this.loadTestMultiply > 1) {
+          rawStructures = this.multiplyDataLoad(rawStructures, this.loadTestMultiply);
         }
+
+        for (let i = 0; i < rawStructures.length; i ++) {
+          structures[i] = structureFactory.make(rawStructures[i]);
+          structures[i].id = i;
+        }
+
         return structures;
       })
   }
