6fd7382218e27c8d4551c57708f56fc2a4d8d457
netlag <n3tlag@outlook.com>
2021-10-19 16:53:34 -0400

    Icons

--- src/js/ui/components/DroidUISchematic.js
+++ src/js/ui/components/DroidUISchematic.js
@@ -28,20 +28,44 @@ export class DroidUISchematic {
               <div><span class="attribute-label">Ambits:</span> ${this.schematic.getAmbits()}</div>
               <div><span class="attribute-label">Energy to Build:</span> Requires ${this.schematic.getEnergyToBuildAmount()} ${this.schematic.getEnergyToBuildDenom()}</div>
               <br>
-              <div><span class="attribute-label">Max Health:</span> ${this.schematic.getHealthMax()}</div>
-              <div><span class="attribute-label">Max Speed:</span> ${this.schematic.getSpeed()}</div>
-              <div><span class="attribute-label">Max Accuracy:</span> ${this.schematic.getAccuracy()}</div>
-              <div><span class="attribute-label">Max Mass:</span> ${this.schematic.getMass()}</div>
-              <div><span class="attribute-label">Max Strength:</span> ${this.schematic.getStrength()}</div>
-              <div><span class="attribute-label">Max Energy Efficiency:</span> ${this.schematic.getEnergyEfficiency()}</div>
+              <div>
+                <img src="/img/icons/icon-health.png" alt="Health Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Health:</span> ${this.schematic.getHealthMax()}
+              </div>
+              <div>
+                <img src="/img/icons/icon-speed.png" alt="Speed Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Speed:</span> ${this.schematic.getSpeed()}
+              </div>
+              <div>
+                <img src="/img/icons/icon-accuracy.png" alt="Accuracy Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Accuracy:</span> ${this.schematic.getAccuracy()}
+              </div>
+              <div>
+                <img src="/img/icons/icon-mass.png" alt="Mass Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Mass:</span> ${this.schematic.getMass()}
+              </div>
+              <div>
+                <img src="/img/icons/icon-strength.png" alt="Strength Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Strength:</span> ${this.schematic.getStrength()}
+              </div>
+              <div>
+                <img src="/img/icons/icon-energy.png" alt="Energy Efficiency Icon" class="structure-card-icon">
+                <span class="attribute-label">Max Energy Efficiency:</span> ${this.schematic.getEnergyEfficiency()}
+              </div>
             </div>
           </div>
           ${(this.schematic.hasFeatureAttack()) ? `
             <div class="nes-container with-title">
               <h3 class="title">Attack Systems</h3>
               <div class="details">
-                <div><span class="attribute-label">Max Melee Attack:</span> ${this.schematic.getMeleeAttack()}</div>
-                <div><span class="attribute-label">Max Range Attack:</span> ${this.schematic.getRangeAttack()}</div>
+                <div>
+                  <img src="/img/icons/icon-attack-melee.png" alt="Melee Attack Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Melee Attack:</span> ${this.schematic.getMeleeAttack()}
+                </div>
+                <div>
+                  <img src="/img/icons/icon-attack-range.png" alt="Range Attack Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Range Attack:</span> ${this.schematic.getRangeAttack()}
+                </div>
               </div>
             </div>
           ` : ''}
@@ -49,8 +73,14 @@ export class DroidUISchematic {
             <div class="nes-container with-title">
               <h3 class="title">Defensive Systems</h3>
               <div class="details">
-                <div><span class="attribute-label">Max Melee Defense:</span> ${this.schematic.getMeleeDefense()}</div>
-                <div><span class="attribute-label">Max Range Defense:</span> ${this.schematic.getRangeDefense()}</div>
+                <div>
+                  <img src="/img/icons/icon-def-melee.png" alt="Melee Defense Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Melee Defense:</span> ${this.schematic.getMeleeDefense()}
+                </div>
+                <div>
+                  <img src="/img/icons/icon-def-range.png" alt="Range Defense Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Range Defense:</span> ${this.schematic.getRangeDefense()}
+                </div>
               </div>
             </div>
           ` : ''}
@@ -58,8 +88,14 @@ export class DroidUISchematic {
             <div class="nes-container with-title">
               <h3 class="title">Engineering Systems</h3>
               <div class="details">
-                <div><span class="attribute-label">Max Build Rate:</span> ${this.schematic.getBuildRate()}</div>
-                <div><span class="attribute-label">Max Restoration Rate:</span> ${this.schematic.getRestorationRate()}</div>
+                <div>
+                  <img src="/img/icons/icon-eng-build.png" alt="Build Rate Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Build Rate:</span> ${this.schematic.getBuildRate()}
+                </div>
+                <div>
+                  <img src="/img/icons/icon-eng-restoration.png" alt="Restoration Rate Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Restoration Rate:</span> ${this.schematic.getRestorationRate()}
+                </div>
               </div>
             </div>
           ` : ''}
@@ -67,9 +103,18 @@ export class DroidUISchematic {
             `<div class="nes-container with-title">
               <h3 class="title">Power Systems</h3>
               <div class="details">
-                <div><span class="attribute-label">Max Generation Rate:</span> ${this.schematic.getGenerationRate()}</div>
-                <div><span class="attribute-label">Max Charge Rate:</span> ${this.schematic.getChargeRate()}</div>
-                <div><span class="attribute-label">Max Drain Rate:</span> ${this.schematic.getDrainRate()}</div>
+                <div>
+                  <img src="/img/icons/icon-power-generate.png" alt="Generation Rate Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Generation Rate:</span> ${this.schematic.getGenerationRate()}
+                </div>
+                <div>
+                  <img src="/img/icons/icon-power-charge.png" alt="Charge Rate Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Charge Rate:</span> ${this.schematic.getChargeRate()}
+                </div>
+                <div>
+                  <img src="/img/icons/icon-power-drain.png" alt="Drain Rate Icon" class="structure-card-icon">
+                  <span class="attribute-label">Max Drain Rate:</span> ${this.schematic.getDrainRate()}
+                </div>
               </div>
             </div>`
           : ''}
