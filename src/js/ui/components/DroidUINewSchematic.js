9e7a573ba778bfbfcdfe81229da9f8d42e146f75
Abstrct <abstract@reactor.lan>
2021-10-21 19:33:11 -0600

    Patent button :D

--- src/js/ui/components/DroidUINewSchematic.js
+++ src/js/ui/components/DroidUINewSchematic.js
@@ -18,12 +18,26 @@ export class DroidUINewSchematic {
               Your browser does not support the canvas element.
             </canvas>
           </div>
+         <div class="nes-container with-title">
+            <div class="nes-field">
+              <label for="new_schematic_name">Name</label>
+              <input type="text" id="new_schematic_name" class="nes-input" value="" >
+            </div>
+            <div class="nes-field">
+              <label for="new_schematic_description">Description</label>
+              <input type="text" id="new_schematic_description" class="nes-input" value="">
+            </div>
+            <div class="row solo-action-wrapper">
+              <div class="col">
+                <a href="" class="nes-btn is-primary nes-btn-fluid">Patent</a>
+              </div>
+            </div>
+          </div>
+
           <div class="nes-container with-title">
             <h3 class="title">Attributes</h3>
             <div class="details">
-              <h4>${this.schematic.getName()}</h4>
-              <p>${this.schematic.getDescription()}</p>
-              <br>
+         
               <div><span class="attribute-label">Ambits:</span>
                 ${this.schematic.hasAmbitWater() ? `
                   <img src="/img/icons/icon-ambit-water.png" alt="Water" title="Water"/> Water
