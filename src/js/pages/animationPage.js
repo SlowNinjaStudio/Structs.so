57a82be72c9a7fd928cf4e02236d58d21f9556e0
netlag <n3tlag@outlook.com>
2021-11-22 15:17:38 -0500

    Station shooting animation

--- src/js/pages/animationPage.js
+++ src/js/pages/animationPage.js
@@ -13,6 +13,7 @@ import {CarShootingAnimator} from "../animations/CarShootingAnimator";
 import {MachineGunDamageAnimator} from "../animations/MachineGunDamageAnimator";
 import {CityShootingAnimator} from "../animations/CityShootingAnimator";
 import {LaserDamageAnimator} from "../animations/LaserDamageAnimator";
+import {StationShootingAnimator} from "../animations/StationShootingAnimator";
 
 const instance = new Instance();
 await instance.init();
@@ -60,42 +61,60 @@ const city2 = DummyUtil.getDummyStructure(
   [FEATURES.DEFENSIVE, FEATURES.ENGINEERING, FEATURES.POWER],
   100
 );
+const station1 = DummyUtil.getDummyStructure(
+  false,
+  [AMBITS.WATER, AMBITS.LAND, AMBITS.SKY, AMBITS.SPACE],
+  [FEATURES.ATTACK, FEATURES.POWER, FEATURES.ENGINEERING],
+  10
+);
+const station2 = DummyUtil.getDummyStructure(
+  false,
+  [AMBITS.SPACE],
+  [FEATURES.DEFENSIVE, FEATURES.ENGINEERING, FEATURES.POWER],
+  10
+);
+
+const backgroundAnimator = new BackgroundAnimator();
 
 const mechShootingAnimator = new MechShootingAnimator();
-const backgroundAnimator = new BackgroundAnimator();
 const shellDamageAnimator = new ShellDamageAnimator();
+
 const carShootingAnimator = new CarShootingAnimator();
 const machineGunDamageAnimator = new MachineGunDamageAnimator();
+
 const cityShootingAnimator = new CityShootingAnimator();
 const laserDamageAnimator = new LaserDamageAnimator();
+
+const stationShootingAnimator = new StationShootingAnimator();
+const antiAirDamageAnimator = new ShellDamageAnimator();
 
 const animationBackground1 = await backgroundAnimator.animate(mech1);
 const animatedMechShooting1 = await mechShootingAnimator.animate(mech1);
 const animationEngineAttack1 = new AnimationEngine('canvas-attack-mech', {flipHorizontally: true});
 animationEngineAttack1.registerAnimatedObjects(animationBackground1);
 animationEngineAttack1.registerAnimatedObjects(animatedMechShooting1);
-animationEngineAttack1.play();
+//animationEngineAttack1.play();
 
 const animationBackground2 = await backgroundAnimator.animate(mech2);
 const shellDamage = await shellDamageAnimator.animate(mech2);
 const animationEngineDefend1 = new AnimationEngine('canvas-defend-mech');
 animationEngineDefend1.registerAnimatedObjects(animationBackground2);
 animationEngineDefend1.registerAnimatedObjects(shellDamage);
-animationEngineDefend1.play();
+// animationEngineDefend1.play();
 
 const animationBackground3 = await backgroundAnimator.animate(car1);
 const animatedCarShooting = await carShootingAnimator.animate(car1);
 const animationEngineAttack2 = new AnimationEngine('canvas-attack-car', {flipHorizontally: true});
 animationEngineAttack2.registerAnimatedObjects(animationBackground3);
 animationEngineAttack2.registerAnimatedObjects(animatedCarShooting);
-animationEngineAttack2.play();
+//animationEngineAttack2.play();
 
 const animationBackground4 = await backgroundAnimator.animate(car2);
 const machineGunDamage = await machineGunDamageAnimator.animate(car2);
 const animationEngineDefend2 = new AnimationEngine('canvas-defend-car');
 animationEngineDefend2.registerAnimatedObjects(animationBackground4);
 animationEngineDefend2.registerAnimatedObjects(machineGunDamage);
-animationEngineDefend2.play();
+//animationEngineDefend2.play();
 
 const animationBackground5 = await backgroundAnimator.animate(city1);
 const animatedCityShooting = await cityShootingAnimator.animate(city1);
@@ -110,6 +129,20 @@ const animationEngineDefend3 = new AnimationEngine('canvas-defend-city');
 animationEngineDefend3.registerAnimatedObjects(animationBackground6);
 animationEngineDefend3.registerAnimatedObjects(laserDamage);
 animationEngineDefend3.play();
+
+const animationBackground7 = await backgroundAnimator.animate(station1);
+const animatedStationShooting1 = await stationShootingAnimator.animate(station1);
+const animationEngineAttack4 = new AnimationEngine('canvas-attack-station', {flipHorizontally: true});
+animationEngineAttack4.registerAnimatedObjects(animationBackground7);
+animationEngineAttack4.registerAnimatedObjects(animatedStationShooting1);
+animationEngineAttack4.play();
+
+const animationBackground8 = await backgroundAnimator.animate(station2);
+const antiAirDamage = await antiAirDamageAnimator.animate(station2);
+const animationEngineDefend4 = new AnimationEngine('canvas-defend-station');
+animationEngineDefend4.registerAnimatedObjects(animationBackground8);
+animationEngineDefend4.registerAnimatedObjects(antiAirDamage);
+animationEngineDefend4.play();
 
 const footer = new Footer();
 footer.init('footer-wrapper');
