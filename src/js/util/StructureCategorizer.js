78449ae6127abb872c4e6a0da04f2c8453e9b7b2
netlag <n3tlag@outlook.com>
2022-01-18 16:02:57 -0500

    STRUCT-54 - Sort structs by most urgent

--- src/js/util/StructureCategorizer.js
+++ src/js/util/StructureCategorizer.js
@@ -4,6 +4,42 @@ export class StructureCategorizer {
 
   /**
    * @param {Structure[]} structures
+   * @param {string} order ASC or DESC
+   */
+  sortByHealth(structures, order = 'ASC') {
+    structures.sort((a, b) => {
+      const healthA = Math.round((a.getHealthCurrent() / a.getHealthMax()) * 100);
+      const healthB = Math.round((b.getHealthCurrent() / b.getHealthMax()) * 100);
+      if (healthA < healthB) {
+        return (order === 'ASC') ? -1 : 1;
+      } else if (healthB < healthA) {
+        return (order === 'ASC') ? 1 : -1;
+      } else {
+        return 0;
+      }
+    })
+  }
+
+  /**
+   * @param {Structure[]} structures
+   * @param {string} order ASC or DESC
+   */
+  sortByBatteryAmount(structures, order = 'ASC') {
+    structures.sort((a, b) => {
+      const amountA = Math.abs(a.getBatteryAmount());
+      const amountB = Math.abs(b.getBatteryAmount());
+      if (amountA < amountB) {
+        return (order === 'ASC') ? -1 : 1;
+      } else if (amountB < amountA) {
+        return (order === 'ASC') ? 1 : -1;
+      } else {
+        return 0;
+      }
+    })
+  }
+
+  /**
+   * @param {Structure[]} structures
    * @return {Structure[][]}
    */
   categorizeByHealth(structures) {
@@ -14,6 +50,8 @@ export class StructureCategorizer {
         damaged.push(structures[i]);
       }
     }
+
+    this.sortByHealth(damaged);
 
     return [damaged];
   }
@@ -41,6 +79,11 @@ export class StructureCategorizer {
         kiloWatt.push(structures[i]);
       }
     }
+
+    this.sortByBatteryAmount(kiloWatt, 'DESC');
+    this.sortByBatteryAmount(megaWatt, 'DESC');
+    this.sortByBatteryAmount(gigaWatt, 'DESC');
+    this.sortByBatteryAmount(teraWatt, 'DESC');
 
     return [kiloWatt, megaWatt, gigaWatt, teraWatt];
   }
